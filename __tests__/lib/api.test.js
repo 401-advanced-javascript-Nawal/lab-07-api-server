9a75769fc709035f41f370be5a21e616a3128298
Nsuliman <nsuliman88@gmail.com>
2020-01-29 13:25:34 +0200

    testing methods and routes are Done

--- __tests__/lib/api.test.js
+++ __tests__/lib/api.test.js
@@ -41,14 +41,64 @@ describe(' Server Testing ', () => {
 
     /**************** Routes ****************/
 
+    // categories
     it('Routes , Valid route test  ', () => {
         return mockRequest
             .get('/api/v1/categories')
             .then(data => {
                 expect(data.status).toBe(200);
             }).catch(e => console.error(e));
-    }); // 404
+    }); // get
+
+    it('Routes , Valid route test with ID ', () => {
+        return mockRequest
+            .get('/api/v1/categories/1')
+            .then(data => {
+                expect(data.status).toBe(200);
+            }).catch(e => console.error(e));
+    }); // get ID
+
+    it('Routes , Valid route test  ', () => {
+        return mockRequest
+            .post('/api/v1/categories')
+            .then(data => {
+                expect(data.status).toBe(201);
+            }).catch(e => console.error(e));
+    }); // post
+
+    it('Routes , Valid route test  ', () => {
+        return mockRequest
+            .put('/api/v1/categories/1')
+            .then(data => {
+                expect(data.status).toBe(200);
+            }).catch(e => console.error(e));
+    }); // put
+
+    it('Routes , Valid route test  ', () => {
+        return mockRequest
+            .delete('/api/v1/categories/1')
+            .then(data => {
+                expect(data.status).toBe(201);
+            }).catch(e => console.error(e));
+    }); // delete
 
 
+    // Products
+    it('Routes , Valid route test  ', () => {
+        return mockRequest
+            .get('/api/v1/products')
+            .then(data => {
+                expect(data.status).toBe(200);
+            }).catch(e => console.error(e));
+    }); // get
 
+    it('Routes , Valid route test with ID ', () => {
+        return mockRequest
+            .get('/api/v1/products/2')
+            .then(data => {
+                expect(data.status).toBe(200);
+            }).catch(e => console.error(e));
+    }); // get ID
+
+    
 }); // end of server testing 
